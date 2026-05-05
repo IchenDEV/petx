951a4896647145e4e6313a6b70e102f501eb2647
idevlab <idevlab@outlook.com>
2026-05-05 23:14:58 +0800

    Avoid cached Pages asset names

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -5,4 +5,13 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
   publicDir: '../assets',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/petx-[hash].js',
+        chunkFileNames: 'assets/petx-[hash].js',
+        assetFileNames: 'assets/petx-[hash][extname]',
+      },
+    },
+  },
 });
